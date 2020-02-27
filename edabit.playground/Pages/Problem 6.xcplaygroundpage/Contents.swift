0b83061bbff7f3fbd26de3b1c533fdae114f46d0
mdmunifhasan8520 <mdmunifhasan8520@gmail.com>
2020-02-27 16:41:26 +0600

    solve upto problem 6.

---
+++ edabit.playground/Pages/Problem 6.xcplaygroundpage/Contents.swift
@@ -0,0 +1,49 @@
+//: [Previous](@previous)
+import UIKit
+import XCTest
+
+func remainder(_ x: Int, _ y: Int) -> Int {
+    if y>0 || y<0{
+        return x%y
+    } else {
+        return 0
+    }
+}
+
+class SolutionTest: XCTestCase {
+   
+    func test1() {
+         XCTAssertEqual(remainder(7, 2), 1)
+    }
+    func test2() {
+         XCTAssertEqual(remainder(3, 4), 3)
+    }
+    func test3() {
+         XCTAssertEqual(remainder(-9, 45), -9)
+    }
+    func test4() {
+         XCTAssertEqual(remainder(5, 5), 0)
+    }
+    func test5() {
+         XCTAssertEqual(remainder(0, 5), 0)
+    }
+    func test6() {
+         XCTAssertEqual(remainder(0, -1), 0)
+    }
+    func test7() {
+         XCTAssertEqual(remainder(-8, -5), -3)
+    }
+    func test8() {
+         XCTAssertEqual(remainder(13, 5), 3)
+    }
+    func test9() {
+         XCTAssertEqual(remainder(-8, 0), 0)
+    }
+    func test10() {
+         XCTAssertEqual(remainder(0, 0), 0)
+    }
+}
+
+SolutionTest.defaultTestSuite.run()
+//print("hello")
+//: [Next](@next)
---
+++ edabit.playground/Pages/Test.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var x = 2
+var y = 0
+var z = 0
+
+//if y == 0 {
+//    z = 0
+//}
+//var z = x%y
+//print(z)
+if y>0 {
+    z = x%y
+} else {
+    z = 0
+}
+print(z)
+
+//: [Next](@next)
